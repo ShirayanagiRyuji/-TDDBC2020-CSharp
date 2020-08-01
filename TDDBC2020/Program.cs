6f14a7be798d358bde79ac3b4c376c0b2de92d84
ShirayanagiRyuji <50043785+ShirayanagiRyuji@users.noreply.github.com>
2020-08-01 12:43:27 +0900

    Add files via upload

---
+++ TDDBC2020/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TDDBC2020
+{
+    /// <summary>
+    /// TDDBC2020
+    /// お題：
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UnitTestProject_TDDBC2020/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject_TDDBC2020
+{
+    /// <summary>
+    /// TDDBC2020 Ž©“®ƒeƒXƒg
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
